33fbe1e289c281ad5e2500333783bc56f7efa8d1
Rafferty Uy <rafferty.uy@gmail.com>
2024-11-01 18:42:28 +0800

    add dailyeletedemoresources automation

---
+++ RazAutomation/DailyDeleteDemoResources/run.ps1
@@ -0,0 +1,13 @@
+# Input bindings are passed in via param block.
+param($Timer)
+
+# Delete resources with naming conventions like 'prefix-demo-deleteme'
+$resources = Get-AzResourceGroup | Where-Object ResourceGroupName -like '*-deleteme'
+
+Foreach ($resource in $resources)
+{
+    Write-Host "Deleting $($resource.ResourceGroupName)..."
+    Remove-AzResourceGroup -Name $resource.ResourceGroupName -Force
+}
+
+Write-Host "Done deleting all '-deleteme' Resource Groups..."
